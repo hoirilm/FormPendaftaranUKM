6a182de70121b9388a51bd4476b9919d0139fa97
hoiril mochtar <horimo.me@gmail.com>
2019-10-30 11:13:42 +0700

    menambahkan file kedalam project

---
+++ src/projectakhir/tampung.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectakhir;
+
+/**
+ *
+ * @author anonymous
+ */
+public class tampung {
+    public static int baris = 0;
+    public static String NIM[] = new String[999999];
+    public static String NAMA[] = new String[999999];
+    public static String ALAMAT[] = new String[999999];
+    public static String TEMPAT[] = new String[999999];
+    public static String TANGGAL[] = new String[999999];
+    public static String JENKEL[] = new String[999999];
+    public static String NO_HP[] = new String[999999];
+    public static String EMAIL[] = new String[999999];
+    public static String PRODI[] = new String[999999];
+    public static String MINAT[] = new String[999999];
+}
